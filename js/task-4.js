60e61f0400532d7740cb1161f2b9d58d3751ba1f
HP <zontiksm@ukr.net>
2024-07-15 12:14:23 +0300

    add index file

--- js/task-4.js
+++ js/task-4.js
@@ -4,14 +4,13 @@ function getShippingCost(country) {
     let price;
  switch (country) {
 
-     case "China": price = 100; return `Shipping to ${country}China will cost ${price} credits`
-    break ;
-     case "Chile": price = 250; return  `Shipping to ${country}China will cost ${price} credits`
-     break ;
-   case "Australia" : price = 170; return  `Shipping to ${country}China will cost ${price} credits`
-         break;
-       case "Jamaica" : price = 120; return `Shipping to ${country}China will cost ${price} credits`
-     break;
+   case "China": price = 100; return `Shipping to ${country} will cost ${price} credits`;
+   
+   case "Chile": price = 250; return `Shipping to ${country} will cost ${price} credits`;
+   
+   case "Australia": price = 170; return `Shipping to ${country} will cost ${price} credits`;
+   
+       case "Jamaica" : price = 120; return `Shipping to ${country} will cost ${price} credits`;
 
      default : return "Sorry, there is no delivery to your country" 
 }
